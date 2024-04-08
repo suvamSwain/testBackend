476924f8675ca18c8161e9da846908d74b7d8fc6
Suvam <suvamgpt8@gmail.com>
2024-04-09 02:10:52 +0530

    changed vercel

--- backend/settings.py
+++ backend/settings.py
@@ -27,8 +27,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'https://customsitemadebyme.netlify.app',
-    '.vercel.app'
+    'https://customsitemadebyme.netlify.app'
 ]
 
 
