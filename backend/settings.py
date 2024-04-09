2de4868af1fb3059bfb5ba078061611944b7a340
Suvam <suvamgpt8@gmail.com>
2024-04-09 07:47:40 +0530

    just test

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'https://customsitemadebyme.netlify.app'
+    'https://customsitemadebyme.netlify.app',
+    'vercel.app'
 ]
 
 
